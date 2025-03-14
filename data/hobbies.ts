b93c4cd08e589a6051f023700d20bd10886c958b
Sanskargupta0 <sanskar362002@gmail.com>
2025-03-14 11:51:11 +0530

    Init: Portfolio Website

---
+++ data/hobbies.ts
@@ -0,0 +1,44 @@
+import type { Hobby } from './types';
+
+export const hobbies: Hobby[] = [
+    {
+        title: {
+            en: 'Skateboarding',
+            de: 'Skateboard'
+        },
+        icon: 'skateboard',
+        emoji: '🛹'
+    },
+    {
+        title: {
+            en: 'Gaming',
+            de: 'Jeux-vidéo'
+        },
+        icon: 'joystick',
+        emoji: '🕹️'
+    },
+    {
+        title: {
+            en: 'Programming',
+            de: 'Programmation'
+        },
+        icon: 'keyboard',
+        emoji: '⌨️'
+    },
+    {
+        title: {
+            en: 'Computer Science',
+            de: 'Informatique'
+        },
+        icon: 'desktop-computer',
+        emoji: '🖥️'
+    },
+    {
+        title: {
+            en: 'Running',
+            de: 'Course à pied'
+        },
+        icon: 'man-running',
+        emoji: '🏃‍♂️'
+    }
+];
---
+++ pages/resume.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <embed
+            src="/Sanskar-Gupta-resume-2025.pdf"
+            type="application/pdf"
+            style="min-height: 100vh; width: 100%"
+        />
+    </div>
+</template>
